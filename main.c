c44eab2342975158d50259fb83feacb3e859817f
Mili Mil <fmaric.os@gmail.com>
2020-08-26 15:11:53 +0200

    Add function for updating conditional flags and function for sign extending; Implement ADD instruction

--- main.c
+++ main.c
@@ -65,6 +65,27 @@ enum{
 	OP_TRAP		 // execute trap
 };
 
+
+
+uint16_t sign_extend(uint16_t x, int bit_count){
+	if((x >> (bit_count - 1)) & 1){
+		x |= (0xFFF << bit_count);
+	}
+	return x;
+}
+
+void update_flags(uint16_t r){
+	if(reg[r] == 0){
+		reg[R_COND] = FL_ZRO;
+	}
+	else if(reg[r] >> 15 ){ // 1 in the left-most bit indicates negative
+		reg[R_COND] = FL_NEG;
+	}
+	else{
+		reg[R_COND] = FL_POS;
+	}
+}
+
 uint16_t check_key(){
 	fd_set readfds;
 	FD_ZERO(&readfds);
@@ -182,7 +203,23 @@ int main(int argc, const char* argv[1]){
 
 		switch(op){
 			case OP_ADD:
-				// TODO
+				// destination register DR
+				uint16_t r0 = (instr >> 9) & 0x7;
+				// first operand SR1
+				uint16_t r1 = (instr >> 6) & 0x7;
+				// check immediate mode
+				uint16_t imm_flag = (instr >> 5) & 0x1;
+
+				if(imm_flag){
+					uint16_t imm5 = sign_extend(instr & 0x1F, 5);
+					reg[r0] = reg[r1] + imm5;
+				}
+				else{
+					uint16_t r2 = instr & 0x7;
+					reg[r0] = reg[r1] + reg[r2];
+				}
+				update_flags(r0);
+
 				break;
 
 			case OP_AND:
