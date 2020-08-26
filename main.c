f12ca00b8b50864f837482487c7c872fd8807d6d
Mili Mil <fmaric.os@gmail.com>
2020-08-26 23:37:25 +0200

    Implement NOT insturction

--- main.c
+++ main.c
@@ -241,7 +241,12 @@ int main(int argc, const char* argv[1]){
 				break;
 
 		  case OP_NOT:
-				// TODO
+				uint16_t r0 = (instr >> 9) & 0x7;
+				uint16_t r1 = (instr >> 6) & 0x7;
+
+				reg[r0] = ~reg[r1];
+				update_flags(r0);
+
 				break;
 
 			case OP_BR:
