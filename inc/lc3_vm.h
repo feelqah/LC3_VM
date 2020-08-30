46fc8fe28f3d03664b953d0074fce873ab12eb7a
Mili Mil <fmaric.os@gmail.com>
2020-08-30 23:39:01 +0200

    Add TRAP codes

--- inc/lc3_vm.h
+++ inc/lc3_vm.h
@@ -55,6 +55,15 @@ enum{
 	OP_TRAP			// execute trap
 };
 
+enum{
+    TRAP_GETC  = 0x20, // get character from keyboard, not echoed to terminal
+    TRAP_OUT   = 0x21, // output a character
+    TRAP_PUTS  = 0x22, // output a word string
+    TRAP_IN    = 0x23, // get character from keyboard, echoed to terminal
+    TRAP_PUTSP = 0x24, // output a byte string
+    TRAP_HALT  = 0x25  // halt the program
+};
+
 /* Function declarations */
 
 int read_image(const char* image_path);
