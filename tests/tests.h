6b13970a9bf63a3258845f1199576493fd3d856b
Mili Mil <fmaric.os@gmail.com>
2020-09-02 16:18:24 +0200

    Add test declarations to tests.h

--- tests/tests.h
+++ tests/tests.h
@@ -1,2 +1,24 @@
+//TODO: Add documentation
+
 int add(char* image_path[]);
+int and(char* image_path[]);
+int not(char* image_path[]);
+int br(char* image_path[]);
+int jmp(char* image_path[]);
+int jsr(char* image_path[]);
+int ld(char* image_path[]);
+int ldi(char* image_path[]);
+int ldr(char* image_path[]);
+int lea(char* image_path[]);
+int st(char* image_path[]);
+int sti(char* image_path[]);
+int str(char* image_path[]);
+int trap_getc(char* image_path[]);
+int trap_out(char* image_path[]);
+int trap_puts(char* image_path[]);
+int trap_in(char* image_path[]);
+int trap_putsp(char* image_path[]);
+int trap_halt(char* image_path[]);
+int res(char* image_path[]);
+int rti(char* image_path[]);
 
