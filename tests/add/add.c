798ad1986dd94fda1d30b91812dea25bbb7a3583
Mili Mil <fmaric.os@gmail.com>
2020-09-03 11:44:06 +0200

    Change comments in add test

--- tests/add/add.c
+++ tests/add/add.c
@@ -4,19 +4,18 @@
 #include "lc3_vm.h"
 
 int add(char* image_path[]){
-    int ret_val = 0;
     /* load object asm test file
        it contains the following asm code in binary:
             ADD R0, R0, 5
     */
-
-    int expected_result = reg[R_R0] + 5;
+    int ret_val = 0;
+    int expected_result = reg[R_R0] + 5; // R0 can already contain some value
 
     lc3_vm(2, image_path); // passing 2 instead of argc
 
     printf("ADD test\t");
 
-    // check if R0 contains 1
+    // check if R0 contains expected result
     if(reg[R_R0] == expected_result){
         // pass
         printf("[PASS]\n");
