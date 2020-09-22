b35fecbac549cbd915db6b6d6e7bf75519b1a8df
Mili Mil <fmaric.os@gmail.com>
2020-09-22 15:14:47 +0200

    Cleanup tests.h

--- tests/tests.h
+++ tests/tests.h
@@ -19,24 +19,4 @@ int trap_puts(char* image_path[]);
 int trap_in(char* image_path[]);
 int trap_putsp(char* image_path[]);
 int trap_halt(char* image_path[]);
-/*
-int not(char* image_path[]);
-int br(char* image_path[]);
-int jmp(char* image_path[]);
-int jsr(char* image_path[]);
-int ld(char* image_path[]);
-int ldi(char* image_path[]);
-int ldr(char* image_path[]);
-int lea(char* image_path[]);
-int st(char* image_path[]);
-int sti(char* image_path[]);
-int str(char* image_path[]);
-int trap_getc(char* image_path[]);
-int trap_out(char* image_path[]);
-int trap_puts(char* image_path[]);
-int trap_in(char* image_path[]);
-int trap_putsp(char* image_path[]);
-int trap_halt(char* image_path[]);
-int res(char* image_path[]);
-int rti(char* image_path[]);
-*/
+
